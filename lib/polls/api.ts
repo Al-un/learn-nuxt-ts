b5ceb07f6e09396581b4b7a874b6e14ade17252d
Al-un <alun.sng@gmail.com>
2019-03-10 20:03:47 +0100

    Polls: models & api definition

---
+++ lib/polls/api.ts
@@ -0,0 +1,37 @@
+import { Poll } from './models';
+
+/**
+ * Dummy polls
+ */
+const DUMMY_POLLS: Poll[] = [
+  {
+    id: 1,
+    topic: 'Which framework are you using?',
+    choices: [
+      { id: 1, count: 0, pollId: 1, text: 'NuxtJS' },
+      { id: 2, count: 0, pollId: 1, text: 'Plain VueJS' },
+      { id: 3, count: 0, pollId: 1, text: 'Angular' },
+      { id: 4, count: 0, pollId: 1, text: 'React' }
+    ]
+  },
+  {
+    id: 2,
+    topic: 'What is your OS?',
+    choices: [
+      { id: 5, count: 0, pollId: 2, text: 'Windows' },
+      { id: 6, count: 0, pollId: 2, text: 'Linux' },
+      { id: 7, count: 0, pollId: 2, text: 'MacOS' }
+    ]
+  }
+];
+
+/**
+ * Load polls with associated choices. Votes are not loaded here.
+ *
+ * It uses a timeout to simulate server response time
+ */
+export const loadPolls = async (): Promise<Poll[]> => {
+  return new Promise<Poll[]>(resolve =>
+    setTimeout(() => resolve(DUMMY_POLLS), 500)
+  );
+};
---
+++ lib/polls/models.ts
@@ -0,0 +1,40 @@
+/**
+ * A vote for a given choice
+ */
+export class Vote {
+  public constructor(
+    public id: number,
+    public choiceId: number,
+    public comment?: string
+  ) {}
+}
+
+/**
+ * A choice to vote for within a Poll
+ */
+export class Choice {
+  public count: number;
+
+  public constructor(public id: number, public pollId: number, public text: string) {
+    this.count = 0;
+  }
+}
+
+/**
+ * A topic with which user is offered multiple choices to vote for
+ */
+export class Poll {
+  public choices: Choice[];
+
+  public constructor(public id: number, public topic: string, choices?: Choice[]) {
+    this.choices = choices !== undefined ? choices : [];
+  }
+}
+
+/**
+ * An intention of voting a given choice with an optional comment
+ */
+export interface ChoiceVote {
+  choiceId: number;
+  comment?: string;
+}
